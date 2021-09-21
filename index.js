1f89006559061ba1c4bfbfb835e3128f0e7e5b8e
Alejandro Marcano <+>
2021-09-21 00:02:58 -0400

    completd assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 31
+let num2 = 2
+let multiply
+multiply = num1 * num2;
+
+
+
+let mod;
+let num3 = 19
+let num4 = 5
+mod = num3 % num4;
+
+let max;
+max = (1,2,3,4,20);
+max.max()
+
+let random;
+random = Math.floor(Math.random() * 10) + 1;
 
 // Write your code here
